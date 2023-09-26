7c7bd8a8beeb5648db0cc487bc376589564532fb
Juan Veras <juan@vvssistemas.com.br>
2023-09-26 20:03:55 -0300

    Utilizando Serviços de Dominio

---
+++ NerdStoreCatalogo.Domain/Estoques/IEstoqueService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStoreCatalogo.Domain.Estoques
+{
+    public interface IEstoqueService : IDisposable
+    {
+        Task<bool> DebitarEstoque(Guid idProduto, int quantidade);
+        Task<bool> ReporEstoque(Guid idProduto, int quantidade);
+    }
+}
